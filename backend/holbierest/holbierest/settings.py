79e22c4547423b700466b7528f76f4ccf5574b4f
HolbieEnjoyer <8698@holbertonstudents.com>
2024-10-09 20:49:21 +0400

    Implement API throttling

--- backend/holbierest/holbierest/settings.py
+++ backend/holbierest/holbierest/settings.py
@@ -139,6 +139,16 @@ REST_FRAMEWORK = {
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 8,
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '20/minute'
+    },
 }
 
 DJOSER = {
