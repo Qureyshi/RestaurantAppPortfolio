4c7b40772d5a9ccc0c608edb291b1f4b72ec7ef9
HolbieEnjoyer <8698@holbertonstudents.com>
2024-10-09 20:10:53 +0400

    Implement Menu Management Endpoints

--- backend/holbierest/rest_api/views.py
+++ backend/holbierest/rest_api/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 
-# Create your views here.
+
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != 'GET':
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    search_fields = ['title', 'category__title']
+    ordering_fields = ['price', 'title', 'category__title']
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != 'GET':
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != 'GET':
+            permission_classes = [IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
